283fce7b81d9b0a88d6135099e0a26221821292a
Zympidis <anastasios.zympidis@gmail.com>
2021-03-05 00:15:59 +0200

    Rename feature package

---
+++ app/src/main/java/app/placesautocomplete/remoterepository/RemoteAutocompleteRepository.kt
@@ -0,0 +1,8 @@
+package app.placesautocomplete.remoterepository
+
+import app.placesautocomplete.remoterepository.model.AutocompleteResponse
+
+interface RemoteAutocompleteRepository {
+
+    suspend fun getPlaces(userInput: String): AutocompleteResponse
+}
--- app/src/main/java/app/propertyad/remoterepository/RemoteAutocompleteRepository.kt
+++
@@ -1,8 +0,0 @@
-package app.propertyad.remoterepository
-
-import app.propertyad.remoterepository.model.AutocompleteResponse
-
-interface RemoteAutocompleteRepository {
-
-    suspend fun getPlaces(userInput: String): AutocompleteResponse
-}
