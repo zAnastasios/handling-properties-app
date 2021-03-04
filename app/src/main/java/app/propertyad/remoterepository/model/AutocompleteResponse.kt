b80e736a237b3db2e6615ae245ee3017db57d40d
Zympidis <anastasios.zympidis@gmail.com>
2021-03-05 00:08:50 +0200

    Add AutocompleteResponse.kt to map API response

---
+++ app/src/main/java/app/propertyad/remoterepository/model/AutocompleteResponse.kt
@@ -0,0 +1,11 @@
+package app.propertyad.remoterepository.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AutocompleteResponse(
+    @Json(name = "placeId") val placeId: String,
+    @Json(name = "mainText") val mainLocationDescription: String,
+    @Json(name = "secondaryText") val secondaryLocationDescription: String
+)
