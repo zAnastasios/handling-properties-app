e3c601aef8c2e8b541f7bdcbf55ee99e1584a5b2
Zympidis <anastasios.zympidis@gmail.com>
2021-03-06 19:49:22 +0200

    Add DI for OkHttp client and Retrofit

---
+++ app/src/main/java/app/di/AppModule.kt
@@ -0,0 +1,35 @@
+package app.di
+
+import app.devchallenge.propertyhandling.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        return loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpBuilder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            okHttpBuilder.addInterceptor(httpLoggingInterceptor)
+        }
+        return okHttpBuilder.build()
+    }
+}
