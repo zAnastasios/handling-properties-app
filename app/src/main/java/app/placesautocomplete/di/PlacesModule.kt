6eee39c622641cd7dbc1a8f24be3ac2d4f163108
Zympidis <anastasios.zympidis@gmail.com>
2021-03-07 19:44:09 +0200

    Add DI for the Autocomplete Service and Autocomplete repository

--- app/src/main/java/app/placesautocomplete/di/PlacesModule.kt
+++ app/src/main/java/app/placesautocomplete/di/PlacesModule.kt
@@ -1,6 +1,9 @@
 package app.placesautocomplete.di
 
 import app.devchallenge.propertyhandling.BuildConfig
+import app.placesautocomplete.remoterepository.AutocompleteService
+import app.placesautocomplete.remoterepository.RemoteAutocompleteRepository
+import app.placesautocomplete.remoterepository.RemoteAutocompleteRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,6 +11,7 @@ import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -23,4 +27,14 @@ object PlacesModule {
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
+
+    @Singleton
+    @Provides
+    fun providesAutocompleteService(retrofit: Retrofit): AutocompleteService =
+        retrofit.create()
+
+    @Singleton
+    @Provides
+    fun providesAutocompleteRepository(autocompleteService: AutocompleteService): RemoteAutocompleteRepository =
+        RemoteAutocompleteRepositoryImpl(autocompleteService = autocompleteService)
 }
