6663cd473ada99752cae6728350c91281dee6122
Zympidis <anastasios.zympidis@gmail.com>
2021-03-07 19:40:53 +0200

    Refactor wrong Model for API response

--- app/src/main/java/app/placesautocomplete/remoterepository/model/AutocompleteResponse.kt
+++ app/src/main/java/app/placesautocomplete/remoterepository/model/AutocompleteResponse.kt
@@ -5,6 +5,11 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class AutocompleteResponse(
+    @Json(name = "places") val places: List<PlacesResponse>
+)
+
+@JsonClass(generateAdapter = true)
+data class PlacesResponse(
     @Json(name = "placeId") val placeId: String,
     @Json(name = "mainText") val mainLocationDescription: String,
     @Json(name = "secondaryText") val secondaryLocationDescription: String
